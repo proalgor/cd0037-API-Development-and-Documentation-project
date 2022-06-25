0fd05cbcde133509bbe915615adc5d214daadaef
franc <onyishif@gmail.com>
2022-06-25 10:35:06 +0100

    setup env

--- backend/models.py
+++ backend/models.py
@@ -2,9 +2,12 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 database_name = 'trivia'
-database_path = 'postgres://{}/{}'.format('localhost:5432', database_name)
+database_path = '{}/{}'.format(config['DB_URL'], database_name)
 
 db = SQLAlchemy()
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -15,7 +18,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "{}/{}".format(config['DB_URL'], self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
